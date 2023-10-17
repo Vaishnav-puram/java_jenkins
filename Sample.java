6da52de06b54ee16e310873ab9cd7b837dfea784
Vaishnav Puram@DESKTOP-1GKDG5O <puramv307@gmail.com>
2023-10-17 07:46:47 +0530

    sample java

---
+++ Sample.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+class Sample{
+    public static void main(String[] args){
+        System.out.println("This is java program....");
+        System.out.println("Hello Vaishnav!");
+        System.out.println(new Date());
+    }
+}
